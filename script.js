e6efd1f57d3fcc5ad113dc04032baf19dd1880c6
ShivaBagri <shiva.sb.214@gmail.com>
2026-04-14 17:56:38 -0400

    script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function sendMessage(event) {
     event.preventDefault();
     alert("Message sent successfully!");
 }
+
+/* Scroll Animation for Cards */
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < triggerBottom) {
+            card.classList.add("show");
+        }
+    });
+});
